41c053397151beaed3ff829fc9b3cd3c6833acb9
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-07-08 16:15:21 +0200

    v.0.1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,40 @@
-myReset() {
+function myReset() {
     console.log("reset")
-    document.getElementById("counter").reset()
+    document.getElementById("result").innerHTML = "0"
 }
 let x = 0
-plusOne() {
+function plusOne() {
     console.log("+1")
-    x+=1
-    document.getElementById("result").innerHTML = "x"
+    x += 1
+    document.getElementById("result").innerHTML = x
 }
-minusOne() {
+function minusOne() {
     console.log("-1")
-    x-=1
-    document.getElementById("result").innerHTML = "x"
+    x -= 1
+    document.getElementById("result").innerHTML = x
 }
-plusTen() {
+function plusTen() {
     console.log("-1")
-    x+=10
-    document.getElementById("result").innerHTML = "x"
+    x += 10
+    document.getElementById("result").innerHTML = x
+}
+function minusTen() {
+    console.log("-10")
+    x -= 10
+    document.getElementById("result").innerHTML = x
+}
+function plusHundred() {
+    console.log("+100")
+    x += 100
+    document.getElementById("result").innerHTML = x
+}
+function minusHundred() {
+    console.log("-100")
+    x -= 100
+    document.getElementById("result").innerHTML = x
+}
+function double() {
+    console.log("*2")
+    x *= 2
+    document.getElementById("result").innerHTML = x
 }
